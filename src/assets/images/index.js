4d18d4f5c012b5019d20a1bf9db95908bb87807a
vanshsmgupta <vanshguptavsg@gmail.com>
2023-12-24 20:09:09 +0530

    Portfolio 3d

---
+++ src/assets/images/index.js
@@ -0,0 +1,21 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+import rotary from './rotary.jpeg'
+import poornima from './poor.png'
+import kendriya from './kendriya.png'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo,
+    rotary,
+    poornima,
+    kendriya
+}
---
+++ src/constants/index.js
@@ -0,0 +1,228 @@
+import { kendriya, meta, poornima, rotary, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    // {
+    //     imageUrl: redux,
+    //     name: "Redux",
+    //     type: "State Management",
+    // },
+    // {
+    //     imageUrl: sass,
+    //     name: "Sass",
+    //     type: "Frontend",
+    // },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    // {
+    //     imageUrl: typescript,
+    //     name: "TypeScript",
+    //     type: "Frontend",
+    // }
+];
+
+export const experiences = [
+    {
+        title: "10th Standard",
+        company_name: "Rotary Integrated Secondary School",
+        icon: rotary,
+        iconBg: "#accbe1",
+        date: "March 2017 - April 2018",
+        points: [
+            "Consistently achieved top grades, demonstrating a strong commitment to academic success in various subjects.",
+            "Actively engaged in class activities, showcasing leadership skills through group projects and extracurricular involvement.",
+            "Participated in diverse extracurricular activities, contributing to a well-rounded education and fostering personal growth.",
+            "Demonstrated strong communication skills through presentations, discussions, and collaboration with peers and teachers.",
+        ],
+    },
+    {
+        title: "12th Standard",
+        company_name: "Kendriya Vidhalaya",
+        icon: kendriya,
+        iconBg: "#fbc3bc",
+        date: "March 2019 - April 2020",
+        points: [
+            "Top-tier academic performance, excelling in challenging 12th-grade courses.",
+            "Leadership in school projects, showcasing initiative, organizational skills.",
+            "Applied critical thinking to solve complex problems effectively.",
+            "Contributed to research projects, fostering a passion for exploration and innovation.",
+        ],
+    },
+    {
+        title: "B.Tech Information Technology",
+        company_name: "Poornima College Of Engineering",
+        icon: poornima,
+        iconBg: "#b7e4c7",
+        date: "May 2020 - Present",
+        points: [
+            "Mastered diverse IT concepts, languages, and tools, demonstrating a strong foundation in technology and problem-solving.",
+            "Led successful IT projects, showcasing organizational skills, teamwork, and the ability to meet deadlines effectively.",
+            "Demonstrated commitment to staying current with industry trends and technologies through self-directed learning and professional development.",
+            "Effectively communicated technical concepts, collaborated on team projects, and engaged in constructive problem-solving within a diverse academic environment.",
+        ],
+    },
+    // {
+    //     title: "Full stack Developer",
+    //     company_name: "Meta",
+    //     icon: meta,
+    //     iconBg: "#a2d2ff",
+    //     date: "Jan 2023 - Present",
+    //     points: [
+    //         "Developing and maintaining web applications using React.js and other related technologies.",
+    //         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //         "Implementing responsive design and ensuring cross-browser compatibility.",
+    //         "Participating in code reviews and providing constructive feedback to other developers.",
+    //     ],
+    // },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/vanshsmgupta',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/vansh-gupta-178a60161',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Hotel Hillock Website',
+        description: 'Elegant Hotel Hillock website: Captivating design, user-friendly interface, and seamless functionality for an exceptional online experience.',
+        link: 'https://www.hotelhillock.com/',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Portfolio Website',
+        description: 'Crafted an engaging and personalized portfolio website showcasing my skills, accomplishments, and professional journey. Seamlessly blending design and functionality.',
+        link: 'https://github.com/vanshsmgupta/portfolio',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Hotelier',
+        description: 'Developed Hotelier an integrated platform offering exceptional accommodation and dining experiences. Seamlessly connecting guests with quality rooms and delectable food and beverages for a memorable stay.',
+        link: 'https://github.com/vanshsmgupta/Hotelier',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'HandWriter',
+        description: 'Introducing HandWriter, a transformative project that elegantly converts digital text into authentic and customizable handwritten script, adding a personal touch to communication and design.',
+        link: 'https://github.com/vanshsmgupta',
+    },
+    // {
+    //     iconUrl: estate,
+    //     theme: 'btn-back-black',
+    //     name: 'Real-Estate Application',
+    //     description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+    //     link: 'https://github.com/adrianhajdin/projects_realestate',
+    // },
+    // {
+    //     iconUrl: summiz,
+    //     theme: 'btn-back-yellow',
+    //     name: 'AI Summarizer Application',
+    //     description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+    //     link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    // }
+];
