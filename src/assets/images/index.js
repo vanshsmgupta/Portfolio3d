0487206c1aeb20993709afb751a2902ba97bea17
vanshsmgupta <vanshguptavsg@gmail.com>
2025-05-08 11:24:10 -0500

    Updated the profile and fixed issue with contact page

---
+++ src/assets/images/index.js
@@ -0,0 +1,25 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+import rotary from './rotary.jpeg'
+import poornima from './poor.png'
+import kendriya from './kendriya.png'
+import ac from './ac.png'
+import at from './at.png'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo,
+    rotary,
+    poornima,
+    kendriya,
+    ac,
+    at
+}
---
+++ src/constants/index.js
@@ -0,0 +1,226 @@
+import { kendriya, meta, poornima, rotary, shopify, starbucks, tesla, ac, at } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    // {
+    //     imageUrl: redux,
+    //     name: "Redux",
+    //     type: "State Management",
+    // },
+    // {
+    //     imageUrl: sass,
+    //     name: "Sass",
+    //     type: "Frontend",
+    // },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    // {
+    //     imageUrl: typescript,
+    //     name: "TypeScript",
+    //     type: "Frontend",
+    // }
+];
+
+export const experiences = [
+    {
+        title: "Software Developer",
+        company_name: "American Chase",
+        icon: ac,
+        iconBg: "#000000",
+        date: "May 2024 - Present",
+        points: [
+            "Client: Fannie Mae | Role: Python Developer (Backend & Automation) – Developed and maintained backend services using Python, optimizing performance and ensuring seamless data flow across systems. Automated repetitive tasks and data pipelines using custom Python scripts, improving operational efficiency and reducing manual errors.",
+            "Client: TriWest Healthcare Alliance | Role: Application Developer – Designed and maintained user interface components using TCL/TK scripting to enhance usability and workflow integration. Utilized TOAD for Oracle to perform database queries, analyze data, and support application-level troubleshooting and enhancements.",
+        ],
+    },
+    {
+        title: "Full Stack Developer",
+        company_name: "AbuTimes.com",
+        icon: at,
+        iconBg: "#b7e4c7",
+        date: "Sept 2023 - Feb 2024",
+        points: [
+            "Developed and maintained a responsive news/media website using modern web technologies, ensuring optimal performance and user experience across devices.",
+            "Built and integrated RESTful APIs to handle dynamic content delivery, user authentication, and content management functionality.",
+            "Implemented front-end features using HTML, CSS, JavaScript (and frameworks like React or Vue if applicable), ensuring clean UI and intuitive UX.",
+            "Optimized website for SEO, fast load times, and scalability, resulting in improved traffic and user engagement.",
+        ],
+    },
+    {
+        title: "12th Standard",
+        company_name: "Kendriya Vidhalaya",
+        icon: kendriya,
+        iconBg: "#fbc3bc",
+        date: "March 2019 - April 2020",
+        points: [
+            "Top-tier academic performance, excelling in challenging 12th-grade courses.",
+            "Leadership in school projects, showcasing initiative, organizational skills.",
+            "Applied critical thinking to solve complex problems effectively.",
+            "Contributed to research projects, fostering a passion for exploration and innovation.",
+        ],
+    },
+    // {
+    //     title: "Full stack Developer",
+    //     company_name: "Meta",
+    //     icon: meta,
+    //     iconBg: "#a2d2ff",
+    //     date: "Jan 2023 - Present",
+    //     points: [
+    //         "Developing and maintaining web applications using React.js and other related technologies.",
+    //         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //         "Implementing responsive design and ensuring cross-browser compatibility.",
+    //         "Participating in code reviews and providing constructive feedback to other developers.",
+    //     ],
+    // },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/vanshsmgupta',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/vansh-gupta-178a60161',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Hotel Hillock Website',
+        description: 'Elegant Hotel Hillock website: Captivating design, user-friendly interface, and seamless functionality for an exceptional online experience.',
+        link: 'https://www.hotelhillock.com/',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Portfolio Website',
+        description: 'Crafted an engaging and personalized portfolio website showcasing my skills, accomplishments, and professional journey. Seamlessly blending design and functionality.',
+        link: 'https://github.com/vanshsmgupta/portfolio',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Hotelier',
+        description: 'Developed Hotelier an integrated platform offering exceptional accommodation and dining experiences. Seamlessly connecting guests with quality rooms and delectable food and beverages for a memorable stay.',
+        link: 'https://github.com/vanshsmgupta/Hotelier',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'HandWriter',
+        description: 'Introducing HandWriter, a transformative project that elegantly converts digital text into authentic and customizable handwritten script, adding a personal touch to communication and design.',
+        link: 'https://github.com/vanshsmgupta',
+    },
+    // {
+    //     iconUrl: estate,
+    //     theme: 'btn-back-black',
+    //     name: 'Real-Estate Application',
+    //     description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+    //     link: 'https://github.com/adrianhajdin/projects_realestate',
+    // },
+    // {
+    //     iconUrl: summiz,
+    //     theme: 'btn-back-yellow',
+    //     name: 'AI Summarizer Application',
+    //     description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+    //     link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    // }
+];
